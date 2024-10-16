f48292105996297cc360555a9ed47f64ccd7160e
nothke <nothke@gmail.com>
2024-10-16 14:43:52 +0200

    Formatting fixes

--- scripts/car_controller.gd
+++ scripts/car_controller.gd
@@ -1,5 +1,7 @@
 extends RigidBody
 
+# Exports
+
 export var raycastHeightOffset: float = 0
 const rayLength = 0.6
 
@@ -13,15 +15,17 @@ export var tractionForceMult: float = 10
 export var sidewaysTractionMult: float = 1
 
 export var engineAudioPath: NodePath
-export var timingPath: NodePath 
-export var  countdownPath: NodePath 
+export var timingPath: NodePath
+export var countdownPath: NodePath
 
-export var checkpointSoundPath: NodePath 
-export var countdownSoundPath: NodePath 
-export var finishSoundPath: NodePath 
+export var checkpointSoundPath: NodePath
+export var countdownSoundPath: NodePath
+export var finishSoundPath: NodePath
 
-export var material: Material 
+export var material: Material
 export var bodyNode: NodePath
+
+# Car vars
 
 var torqueMult: float = 10
 
@@ -110,7 +114,7 @@ func _ready():
 	wheels.resize(4)
 	for i in 4:
 		wheels[i] = Wheel.new()
-	
+
 	wheels[0].point = Vector3(-wheelTrack, 0, wheelBase)
 	wheels[1].point = Vector3(wheelTrack, 0, wheelBase)
 	wheels[2].point = Vector3(-wheelTrack, 0, -wheelBase)
@@ -159,13 +163,13 @@ func _ready():
 		debugSplits = float(config.get_value("debug", "splits", 0)) != 0
 	else:
 		print("Couldn't load " + configPath)
-		
+
 	checkpointPassed = -1
-	
+
 	checkpointTimes.resize(CHECKPOINT_NUM)
 	bestCheckpointTimes.resize(CHECKPOINT_NUM)
 	prevBestCheckpointTimes.resize(CHECKPOINT_NUM)
-	
+
 	for i in CHECKPOINT_NUM:
 		checkpointTimes[i] = 0
 		bestCheckpointTimes[i] = 0
@@ -242,7 +246,7 @@ func _physics_process(dt: float) -> void:
 		countdownText.text = str(countdownTime)
 	else:
 		countdownText.text = ""
-		
+
 	# INPUT
 
 	var xInput: float = -1 if Input.is_key_pressed(KEY_A) else (1 if Input.is_key_pressed(KEY_D) else 0)
@@ -254,31 +258,31 @@ func _physics_process(dt: float) -> void:
 		yInput = samples[replaySample].throttle
 
 	smoothSteer = lerp(smoothSteer, xInput, dt * 10)
-	
+
 	smoothThrottle = lerp(smoothThrottle, throttleInput, dt * 10)
-	
+
 	if stageTime < 0:
 		yInput = 0
-		
+
 	var speed: float = linear_velocity.length()
-	
+
 	var spaceState: PhysicsDirectSpaceState = get_world().direct_space_state
-	
+
 	var up: Vector3 = global_transform.basis.y
 	var forward: Vector3 = global_transform.basis.z
 	var right: Vector3 = global_transform.basis.x
-	
+
 	var forwardVelocity: float = forward.dot(linear_velocity)
 	var sidewaysSpeed: float = right.dot(linear_velocity)
-	
+
 	var wheelsOnGround: int = 0
-	
+
 	var tractionPoint: Vector3 = Vector3.ZERO
-	
+
 	var i: int = 0
 	for w in wheels:
 		var wheelPos: Vector3 = to_global(w.point)
-		
+
 		var origin: Vector3 = wheelPos + up * raycastHeightOffset
 		var dest: Vector3 = origin - up * rayLength
 
@@ -289,20 +293,20 @@ func _physics_process(dt: float) -> void:
 		# suspension
 		if grounded:
 			w.wasGrounded = true
-			
+
 			var hitPoint: Vector3 = result["position"]
 			var normal: Vector3 = result["normal"]
 
 			var distFromTarget: float = (dest - hitPoint).length()
-			
+
 			var spring: float = springRate * distFromTarget
-			
+
 			#var traction: Vector3 = normal * (smoothThrottle * tractionForceMult)
 			#add_central_force(traction)
-			
+
 			var veloAtWheel: Vector3 = get_velocity_at_point(origin)
 			var verticalVeloAtWheel: float = up.dot(veloAtWheel)
-			
+
 			var damp: float = -verticalVeloAtWheel * dampRate
 
 			add_force(normal * (spring + damp), hitPoint - transform.origin)
@@ -315,54 +319,54 @@ func _physics_process(dt: float) -> void:
 
 			#line.add_point(hitPoint, black)
 			#line.add_point(hitPoint + normal * 0.1, green)
-			
+
 			wheelsOnGround += 1
-			
+
 			tractionPoint += hitPoint
 
 		if drawParticles and (grounded != w.wasGrounded || prevYInput != yInput):
 			w.dirt.emitting = true
-		
+
 		var localWheelCenter = wheelRoot.to_local(dest + up * 0.3)
 		w.graphical.translation = localWheelCenter
-		
+
 		var wheelRot: Vector3 = Vector3.ZERO
-		
+
 		if i % 2 == 0:
 			wheelRot = Vector3(0, deg2rad(180), 0)
 		else:
 			wheelRot = Vector3(0, deg2rad(0), 0)
-			
+
 		w.graphical.rotation = wheelRot
-		
+
 		if i < 2:
 			w.graphical.rotate(Vector3.UP, -smoothSteer * deg2rad(30))
-			
+
 		if drawParticles:
 			var dirtSpeedFactor = -smoothSteer if i < 2 else 0.0
 			w.dirt.rotation = Vector3(deg2rad(20), atan(-sidewaysSpeed * 0.1 + dirtSpeedFactor), 0)
-		
+
 		w.wasGrounded = grounded
-		
+
 		i += 1
-		
+
 	# var gear: int = int(floor(forwardVelocity / 8))
-	
+
 	var gearPitch: float = repeat(forwardVelocity, 8.0) / 8.0
 	speedPitch = lerp(speedPitch, speed * 0.1 * gearPitch, dt * 10)
-	
+
 	engineAudio.pitch_scale = clamp(lerp(speedPitch, smoothThrottle * 3, 0.5), 0.3, 10)
 
 	# TRACTION
 	if wheelsOnGround > 0:
 		var wheelFactor: float = wheelsOnGround / 4.0
-		
+
 		var midPoint: Vector3 = tractionPoint / wheelsOnGround
-		
+
 		var steeringFactor = clamp(inverse_lerp(0, 5, speed), 0, 1)
-		
+
 		add_torque(-transform.basis.y * xInput * torqueMult * steeringFactor)
-		
+
 		var maxSpeed = maxSpeedKmh / 3.6
 		var tractionMult = 1.0 - ease(abs(forwardVelocity) / maxSpeed, tractionEase)
 		var tractionForce = tractionMult * yInput * tractionForceMult * wheelFactor
@@ -371,34 +375,34 @@ func _physics_process(dt: float) -> void:
 		var sidewaysSign: int = int(sign(sidewaysSpeed))
 		var earlyTraction: float = sat(sideAbs * 2.0) * sat(1 - sideAbs / 20.0) * 10.0
 		var sidewaysTractionFac: float = (earlyTraction + ease(abs(sidewaysSpeed) / maxTraction, sidewaysTractionEase) * maxTraction) * sidewaysSign;
-		
+
 		var sidewaysTraction: Vector3 = -right * sidewaysTractionMult * sidewaysTractionFac
-		
+
 		add_force(forward * tractionForce + sidewaysTraction, midPoint - transform.origin)
-		
+
 	prevYInput = yInput
-	
+
 	if not isReplay:
 		var s = ReplaySample.new()
 		s.t = transform
 		s.time = stageTime
 		s.throttle = throttleInput
-		
+
 		samples.append(replaySample)
-		
+
 # TODO: rename to on_trigger_entered later
 func BodyEntered(body: Node, checkpointIndex: int) -> void:
 	if body == self:
 		print("Entered " + str(checkpointIndex))
-		
+
 		if checkpointPassed + 1 == checkpointIndex:
 			print("Valid checkpoint!")
-			
+
 			checkpointPassed = checkpointIndex;
 			checkpointSound.play();
 
 			checkpointTimes[checkpointIndex] = stageTime;
-			
+
 		if checkpointPassed == 12 && checkpointIndex == 0:
 			stageEnded = true;
 			finishSound.play();
@@ -408,4 +412,4 @@ func BodyEntered(body: Node, checkpointIndex: int) -> void:
 				for i in CHECKPOINT_NUM:
 					prevBestCheckpointTimes[i] = bestCheckpointTimes[i];
 					bestCheckpointTimes[i] = checkpointTimes[i];
-		
+
